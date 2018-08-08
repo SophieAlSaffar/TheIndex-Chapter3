abfb58bf999c83e3dde0eb2a97342c862b5ee703
Sophie AlSaffar <sophie@Sophies-MacBook-Pro.local>
2018-08-08 18:56:16 +0300

    ch3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
 
-// Data
-import authors from "./data";
-
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import axios from "axios";
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors:[],
+      loading: true,
+
+    }
+  }
+componentDidMount()
+{
+  // this authors: is the one in the constructor
+  //this.setState({authors: authors})
+  //console.log(this.state.loading)
+
+  axios.get('https://the-index-api.herokuapp.com/api/authors/')
+  .then(res => res.data)
+  .then(authors => {
+    this.setState({
+      authors: authors,
+      loading: false
+    })
+
+//console.log(this.state.loading)
+})
+
+    .catch(err => console.log(err));
+
+  }
+
   render() {
+    let loads;
+   if(this.state.loading){
+      loads = <h1 align="center">
+                Loading...
+              </h1>
+    }else{
+      loads = <AuthorsList authors= {this.state.authors} />
+    }
     return (
       <div id="app" className="container-fluid">
         <div className="row">
@@ -16,7 +52,7 @@ class App extends Component {
             <Sidebar />
           </div>
           <div className="content col-10">
-            <AuthorsList authors={authors} />
+            {loads}
           </div>
         </div>
       </div>
